2c751f542092fb7504941bda8e3e31b04d208081
Mila Podchasova <Pod4asova20@gmail.com>
2023-07-08 23:27:21 +0300

    Update test_github_page_open.py

--- lesson01/test_github_page_open.py
+++ lesson01/test_github_page_open.py
@@ -1,6 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 class TestGitHubPageOpen:
     def setup(self):
